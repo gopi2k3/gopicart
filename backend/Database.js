fbd739a9deaf4b49f8376e77375438d3b495cad4
Gopi <138962779+gopi2k3@users.noreply.github.com>
2024-08-20 23:02:18 +0530

    Update Database.js

--- backend/Database.js
+++ backend/Database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+console.log(process.env.MONGODB_URI)
 export const connectDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
