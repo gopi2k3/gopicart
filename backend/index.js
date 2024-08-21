d52bc4f53fb3835e2f460f14932237b53440a950
Gopi <138962779+gopi2k3@users.noreply.github.com>
2024-08-21 12:07:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ connectDb();  // Database Connection
 
 // Advanced CORS setup
 const corsOptions = {
-  origin: 'http://localhost:8080',  // Frontend origin
+  origin: 'http://3.107.55.224:8080',  // Frontend origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,  // Allow cookies and other credentials
   optionsSuccessStatus: 204  // Some legacy browsers choke on 204
