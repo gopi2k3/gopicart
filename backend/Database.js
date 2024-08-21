3157590e35fd60e9c793383da0bab4dabaea70cf
Gopi <138962779+gopi2k3@users.noreply.github.com>
2024-08-21 08:04:19 +0530

    Update Database.js

--- backend/Database.js
+++ backend/Database.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 export const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
