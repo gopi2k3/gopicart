42b129aaf379c1f26aaf6db9a8d0537c0d2d48b9
Gopi <138962779+gopi2k3@users.noreply.github.com>
2024-08-21 08:35:41 +0530

    encv

--- backend/Database.js
+++ backend/Database.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path, { dirname } from "path";
 import { fileURLToPath } from 'url';
-import { Paymentrouter } from "./Routes/payment.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,13 +10,12 @@ dotenv.config();  // Load environment variables
 dotenv.config({path:path.join(__dirname,"config/config.env")});  // Load environment variables
 
 
-const MONGODB_URI = `mongodb+srv://gopi3062003:CvZ2NYDjkNdfHLK9@gopi-cart.nyyua.mongodb.net/?retryWrites=true&w=majority&appName=Gopi-Cart`;
 
  // Log the URI to check if it's correct
 
 export const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(MONGODB_URI);  // Directly use the hardcoded URI here
+    const conn = await mongoose.connect(process.env.MONGODB_URI);  // Directly use the hardcoded URI here
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
